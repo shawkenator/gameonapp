fc07b9c962382e9ca6b2486c2c2fd71d14d07f12
shawkenator <shawkenator@gmail.com>
2014-08-29 12:06:27 -0400

    more video craziness

--- public/js/videoresponsive.js
+++ public/js/videoresponsive.js
@@ -2,19 +2,19 @@ var vp=document.createElement('viewport');
 vp.setAttribute('content','width=device-width');
 
 $(document).ready(function(){
-	if ($(document).width() <= 439){	
+	if ($(document).width() <= 359){	
 $('.LimelightEmbeddedPlayer .LimeLightEmbeddedPlayerFlash').attr( 'style', 'width:280px;height:163px' );
 	}	
-	else if ($(document).width() >= 440){
+	else if ($(document).width() >= 360){
 $('.LimelightEmbeddedPlayer .LimeLightEmbeddedPlayerFlash').attr( 'style', 'width:620px;height:349px' );
 	}
 });
 
 $(window).resize(function(){
-	if ($(document).width() <= 439){	
+	if ($(document).width() <= 359){	
 $('.LimelightEmbeddedPlayer .LimeLightEmbeddedPlayerFlash').attr( 'style', 'width:280px;height:163px' );
 	}	
-	else if ($(document).width() >= 440){
+	else if ($(document).width() >= 360){
 $('.LimelightEmbeddedPlayer .LimeLightEmbeddedPlayerFlash').attr( 'style', 'width:620px;height:349px' );
 	}
 });
