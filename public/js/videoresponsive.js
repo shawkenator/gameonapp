55e48ce8548ae078b19618ba9db60213563a9474
shawkenator <shawkenator@gmail.com>
2014-08-29 12:51:13 -0400

    More Ipad Tests

--- public/js/videoresponsive.js
+++ public/js/videoresponsive.js
@@ -1,3 +1,29 @@
+if(navigator.platform === 'iPad') {
+            window.onorientationchange = function() {
+
+            var orientation = window.orientation;
+
+            // Look at the value of window.orientation:
+
+            if (orientation === 0) {
+            	$('.LimelightEmbeddedPlayer .LimeLightEmbeddedPlayerFlash').attr( 'style', 'width:280px;height:163px' );
+
+            } else if (orientation === 90) {
+            	$('.LimelightEmbeddedPlayer .LimeLightEmbeddedPlayerFlash').attr( 'style', 'width:620px;height:349px' );
+
+            } else if (orientation === -90) {
+             	$('.LimelightEmbeddedPlayer .LimeLightEmbeddedPlayerFlash').attr( 'style', 'width:620px;height:349px' );
+
+            } else if (orientation === 180) {
+            	$('.LimelightEmbeddedPlayer .LimeLightEmbeddedPlayerFlash').attr( 'style', 'width:280px;height:163px' );
+
+            }
+          }       
+        }
+
+/*
+
+
 $(document).ready(function(){
 	if ($(document).width() <= 359){	
 $('.LimelightEmbeddedPlayer .LimeLightEmbeddedPlayerFlash').attr( 'style', 'width:280px;height:163px' );
@@ -15,3 +41,6 @@ $('.LimelightEmbeddedPlayer .LimeLightEmbeddedPlayerFlash').attr( 'style', 'widt
 $('.LimelightEmbeddedPlayer .LimeLightEmbeddedPlayerFlash').attr( 'style', 'width:620px;height:349px' );
 	}
 });
+
+
+*/
